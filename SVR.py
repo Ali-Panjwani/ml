c68182b05ae1e5d6ca3f55623a1707706786db1d
unknown <alirp366@gmail.com>
2020-04-15 01:20:15 -0700

    Updated SVR.py

--- SVR.py
+++ SVR.py
@@ -10,7 +10,7 @@ import pandas as pd
 # Importing Data
 dataset = pd.read_csv('Position_Salaries.csv')
 X = dataset.iloc[:, 1:2].values
-y = dataset.iloc[:, 2].values
+y = dataset.iloc[:, 2:].values
 
 # # Taking Care of Missing Data
 # imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
@@ -24,18 +24,21 @@ y = dataset.iloc[:, 2].values
 # le = LabelEncoder()
 # y = le.fit_transform(y)
 
-# # Feature Scaling
-# sc = StandardScaler()
-# X = sc.fit_transform(X)
+# Feature Scaling
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
 
 # # Splitting Data Into Training Set And Test Set
 # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Creating The Model
-regressor = SVR(kernel = 'rbf', gamma = 0.1)
+regressor = SVR(kernel = 'rbf')
 regressor.fit(X, y)
 
-y_pred = regressor.predict([[6.5]])
+# y_pred = regressor.predict([[6.5]])
+y_pred = sc_y.inverse_transform(regressor.predict(sc_X.transform(np.array([[6.5]]))))
 
 # # Visualizing The Results
 plt.scatter(X, y, color = 'red')
@@ -43,4 +46,14 @@ plt.plot(X, regressor.predict(X), color = 'blue')
 plt.title('Title')
 plt.xlabel('X Variable')
 plt.ylabel('y Label')
+plt.show()
+
+# Visualising the Regression results (for higher resolution and smoother curve)
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Regression Model)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
 plt.show()
