004985d59ca5f6302d3aa69fd8a40e677c8c44d5
unknown <alirp366@gmail.com>
2020-04-17 05:21:50 -0700

    adding Kernel SVM

--- Kernel_SVM.py
+++ Kernel_SVM.py
@@ -34,7 +34,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, rand
 
 # # Fitting The Model
 from sklearn.svm import SVC
-classifier = SVC(kernel = 'linear', random_state = 0)
+classifier = SVC(kernel = 'rbf', random_state = 0)
 classifier.fit(X_train, y_train)
 
 # Predicting Results
