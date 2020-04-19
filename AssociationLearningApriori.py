3993d16915c0a3c3bdb5aacbdb5fb1554a431df0
unknown <alirp366@gmail.com>
2020-04-19 12:35:36 -0700

    Added reinforcement learning files

---
+++ AssociationLearningApriori.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv('Market_Basket_Optimisation.csv', header = None)
+transacrions = []
+
+for i in range(0, 7501):
+    transacrions.append([str(dataset.values[i, j]) for j in range(0, 20)])
+    
+from apyori import apriori
+rules = apriori(transacrions, min_support = 0.003, min_confidence = 0.2, min_lift = 3, min_length = 2)
+
+# Visualizing The Results
+results = list(rules)
